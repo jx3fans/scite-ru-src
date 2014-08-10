1002451c5dcd78795e4bfe0df3a0fae1f318a939
a <a>
2014-08-10 17:04:17 +0800

    修改，能用vs2010编译

--- src/make_with_VC.bat
+++ src/make_with_VC.bat
@@ -3,12 +3,10 @@ REM for make debug version use:
 REM >make_with_VC.bat DEBUG
 
 setlocal
-set VC7=%ProgramFiles%\Microsoft Visual Studio .NET
-set VC71=%ProgramFiles%\Microsoft Visual Studio .NET 2003
-set VC8=%ProgramFiles%\Microsoft Visual Studio 8
 set VC9=%ProgramFiles%\Microsoft Visual Studio 9.0
+set vc10=%ProgramFiles%\Microsoft Visual Studio 10.0
 set Tools=Common7\Tools
-set path=%VC9%\%Tools%;%VC8%\%Tools%;%VC71%\%Tools%;%VC7%\%Tools%;%path%
+set path=%vc10%\%Tools%;%VC9%\%Tools%;%path%
 call vsvars32.bat
 
 if "%1"=="DEBUG" set parameter1=DEBUG=1
